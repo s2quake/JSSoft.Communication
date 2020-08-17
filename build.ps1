20169688e5a91eaf3d68be01d25b3c8c3b40de6b
s2quake <han0210@netsgo.com>
2020-08-17 16:54:48 +0900

    vscode 빌드 및 실행 설정

--- build.ps1
+++ build.ps1
@@ -1,9 +1,10 @@
 $location = Get-Location
 try {
     Set-Location $PSScriptRoot
-    Invoke-WebRequest -Uri "https://gist.githubusercontent.com/s2quake/57ae08b7598f1f4978d8c50326b4086d/raw/e6030100dee0be68e5c0402619fc1f3e524a8443/build.ps1" -OutFile .\.vscode\build.ps1
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s2quake/build/master/build.ps1" -OutFile .\.vscode\build.ps1
     .\.vscode\build.ps1 -WorkingPath $PSScriptRoot -PropsPath "base.props"
 }
 finally {
+    Remove-Item .\.vscode\build.ps1
     Set-Location $location
 }
