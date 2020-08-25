d1b0c9a35b18a0c78599c56255c204404180cd7e
s2quake <han0210@netsgo.com>
2020-08-25 12:28:59 +0900

    build 설정, 사용하지 않는 파일 제거

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ $location = Get-Location
 try {
     Set-Location $PSScriptRoot
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s2quake/build/master/build.ps1" -OutFile .\.vscode\build.ps1
-    .\.vscode\build.ps1 -WorkingPath $PSScriptRoot -PropsPath "base.props"
+    .\.vscode\build.ps1 -WorkingPath $PSScriptRoot -PropsPath "Directory.Build.props"
 }
 finally {
     Remove-Item .\.vscode\build.ps1
--- gen-ver.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -executionpolicy remotesigned -File gen-ver.ps1
-if not %errorlevel% == 0 pause
