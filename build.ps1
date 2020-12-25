a64d4711c9bc6b52109cd88276f9cbcc21e0a279
s2quake <s2quake@naver.com>
2020-12-25 14:47:47 +0900

    publish 관련 속성 개선

--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@ $location = Get-Location
 try {
     Set-Location $PSScriptRoot
     $buildFile = "./.vscode/build.ps1"
+    $outputPath = "bin"
     $propsPath = (
         "../JSSoft.Library/Directory.Build.props",
         "../JSSoft.Library.Commands/Directory.Build.props",
@@ -9,8 +10,11 @@ try {
     ) | ForEach-Object { "`"$_`"" }
     $propsPath = $propsPath -join ","
     $solutionPath = "./JSSoft.Communication.sln"
+    if (!(Test-Path $outputPath)) {
+        New-Item $outputPath -ItemType Directory
+    }
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s2quake/build/master/build.ps1" -OutFile $buildFile
-    Invoke-Expression "$buildFile $solutionPath $propsPath $args"
+    Invoke-Expression "$buildFile $solutionPath $propsPath -Publish -OutputPath $outputPath $args"
 }
 finally {
     Remove-Item ./.vscode/build.ps1
